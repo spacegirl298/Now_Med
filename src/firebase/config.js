e223b77beb21406233656b98aba83ff0c61beb6e
spacegirl298 <jessicajardim2004@gmail.com>
2026-05-13 18:39:47 +0200

    firbase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+//Connecting Firebase to project
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1X4UJaDWkn8YZ0c7vy8b7MejGNhphy5Y",
+  authDomain: "now-med-edebf.firebaseapp.com",
+  projectId: "now-med-edebf",
+  storageBucket: "now-med-edebf.firebasestorage.app",
+  messagingSenderId: "997992642587",
+  appId: "1:997992642587:web:5be2cea39ea923395be0be"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { auth, db } from './firebase/config'
+console.log('Firebase connected:', auth, db)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
